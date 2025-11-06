b411d57da6deeedb7d6a67b2cdecae7d1437e019
Natália Carvalho de Pinho Joaquim <nati.pinho@gmail.com>
2025-11-06 12:49:45 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Passo 1: MOSTRAR a mensagem de boas-vindas
+console.log("Olá, malta! Bem-vindos à programação!");
+
+// Passo 2: Mostrar a soma (Processamento Simples)
+console.log(20 + 5);
+
+// Passo 3: Mostrar um valor Booleano (revisão da Aula 1)
+console.log(true);
